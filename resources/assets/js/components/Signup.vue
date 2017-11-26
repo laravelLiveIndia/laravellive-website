e59d753675ad162d948996e51fca370b5a79e121
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2017-11-26 22:50:04 +0530

    Very first commit to laracon in

---
+++ resources/assets/js/components/Signup.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-12 col-md-6 offset-md-3 py-60">
+        
+        <form class="form-round">
+            <div class="input-group">
+                <input type="text" class="form-control ls-10" placeholder="E.g. user@example.com">
+                <span class="input-group-btn">
+                <button class="btn text-white" style="background-color: #E57D69;" type="button">Subscribe</button>
+                </span>
+            </div>
+        </form>
+        <p class="text-grey py-10">Subscribe to register your interest and be kept up to date with the latest news.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+let mix = require('laravel-mix');
+mix.js('resources/assets/js/app.js', 'public/js')
+	.styles(['public/vendor/css/core.min.css','public/vendor/css/thesaas.css','public/vendor/css/style.css'],'public/css/vendor.css')
+   	.sass('resources/assets/sass/app.scss', 'public/css')
+   	.combine(['public/vendor/js/core.min.js','public/vendor/js/thesaas.js','public/vendor/js/script.js'],'public/js/vendor.js');
