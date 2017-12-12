3da63c58ef32092fdfc119a648ccb27405acea63
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2017-12-12 13:51:31 +0530

    UI tweaks

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -5,28 +5,29 @@
         <hr>
         <div class="row">
             <div class="col-md-8">
-                <p class="lead text-justify" style="">
+                <p class="lead text-justify fw-400" style="">
                     &nbsp;&nbsp;&nbsp;LaravelLive India is a full-day event being curated with an expected audience of about 300 in New Delhi. Our goal is to bring together bright minds to give talks that are idea-focused, and on a wide range of subjects in PHP Laravel, to foster learning, inspiration and provoke conversations that matter.
                 </p>
-                <p class="lead text-justify mt-40" style="">
+                <p class="lead text-justify mt-40 fw-400" style="">
                     &nbsp;&nbsp;&nbsp;Our Aim is to build larger Open Source community around PHP laravel. We will be guiding the new talent around how to learn and contribute to the open source technologies along with the top notch developers of the Ecosystem.
                 </p>
             </div>
             <div class="col-md-4">
-                <div class="shadow-md rounded-lg bg-white p-10">
-                    <h6 class="titillium text-grey-darker fw-600">Important Dates <span class="pulse"></span> </h6>
-                    <hr class="mt-5 mb-5">
+                <div class="shadow-md rounded-lg bg-white p-10  border-t-4 laravel-brand-border">
+                    <h5 class="titillium text-grey-darker fw-600">Important Dates  </h5>
+                    <hr class="mt-15 mb-15">
 
                     <ul class="text-left titillium" style="line-height: 35px;">
-                        <li><span class="fw-600 laravel-brand-text">Start CFP</span> - Dec 11<sup>th</sup>, 2017</li>
-                        <li><span class="fw-600 laravel-brand-text">End CFP </span> - Jan 10<sup>th</sup>, 2018</li>
-                        <li><span class="fw-600 laravel-brand-text">Talks Announced</span> - Jan 20<sup>th</sup>, 2018</li>
-                        <li><span class="fw-600 laravel-brand-text">Conference</span> - March 17<sup>th</sup>, 2018</li>
+                        <li><span class="fw-600 text-info">Start CFP</span> - Dec 11<sup>th</sup>, 2017 <span class="pulse"></span></li>
+                        <li><span class="fw-600 text-info">End CFP </span> - Jan 10<sup>th</sup>, 2018</li>
+                        <li><span class="fw-600 text-info">Talks Announced</span> - Jan 20<sup>th</sup>, 2018</li>
+                        <li><span class="fw-600 text-info">Conference</span> - March 17<sup>th</sup>, 2018</li>
                     </ul>
                 </div>
-                <div class="shadow-md rounded-lg bg-white p-10 mt-10">
-                    <h6 class="titillium text-grey-darker fw-600">More Information</h6>
-                    <hr class="mt-10 mb-10">
+                <div class="shadow-md rounded-lg bg-white p-10 mt-20  border-t-4 laravel-brand-border">
+
+                    <h5 class="titillium text-grey-darker fw-600">More Information</h5>
+                    <hr class="mt-15 mb-15">
                     <a href="https://twitter.com/laravellivein" class="fw-600 pr-40 social-twitter" target="_blank"><i class="fa fa-twitter"></i> #laravellive</a>
                     <a href="https://www.facebook.com/laravellive" target="_blank" class="fw-600 social-facebook"><i class="fa fa-facebook"></i> laravellive</a>
                 </div>
--- resources/views/sections/venue.blade.php
+++ resources/views/sections/venue.blade.php
@@ -1,4 +1,4 @@
-<section class="section svg-back2 bg-grey-lighter pb-0" id="venue">
+<section class="section svg-back bg-grey-lighter pb-0" id="venue">
                     <header class="section-header">
                        <small><strong><i class="fa fa-map-marker fa-2x"></i></strong></small>
                         <h2 style="" class="text-grey-darkest">Venue</h2>
@@ -8,11 +8,12 @@
                             <div class="card card-bordered card-hover-shadow" style="border:none;">
                                 {{-- <a href="#"><img class="card-img-top" src="assets/img/blog-1.jpg" alt="Card image cap"></a> --}}
                                 <div class="card-block p-50">
-                                  <h4 class="card-title"><a href="#">Lal Bahadur Shastri Institute of Management</a></h4>
-                                  <p class="card-text">
+
+                                  <h5 class="card-title"><a href="#"> <span class="pulse"></span>Lal Bahadur Shastri Institute of Management</a></h5>
+                                  <p class="card-text pl-20">
                                       11/07, Dwarka Sector 11, Near Metro Station, <br>Sector 11 Dwarka, Dwarka, <br>New Delhi, Delhi 110075
                                   </p>
-                                  <a class="fw-600 fs-12" href="https://goo.gl/maps/vcUrHgFxMqF2" target="_blank">View on maps <i class="fa fa-chevron-right fs-9 pl-8"></i></a>
+                                  <a class="fw-600 fs-12 pl-20" href="https://goo.gl/maps/vcUrHgFxMqF2" target="_blank">View on maps <i class="fa fa-chevron-right fs-9 pl-8"></i></a>
                                 </div>
                               </div>
                         </div>
