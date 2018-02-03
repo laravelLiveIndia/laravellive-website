d0c99ed360cb20ba576b9cd54a3629da7ba0fc2b
Fahad Khan <fahadkhan1740@outlook.com>
2018-02-03 17:35:00 +0530

    added vaibhav in speakers

--- resources/views/sections/speakers.blade.php
+++ resources/views/sections/speakers.blade.php
@@ -8,7 +8,7 @@
                     <div class="row gap-y">
                         <div class="col-12 col-md-3 col-lg-4 team-2">
                             <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
-                            <a href="https://twitter.com/freekmurze">
+                            <a href="https://murze.be/">
                                 <img class="shadow-lg" src="https://avatars3.githubusercontent.com/u/483853?v=3&s=150" alt="...">
                             </a>
                             <h5>Freek Van der Herten <small>Partner and developer at <a href="https://spatie.be/en">Spatie</a></small></h5>
@@ -44,67 +44,23 @@
                                 </div>
                             </div>
                         </div>
-
-                       {{-- 
-
-                        <div class="col-12 col-md-6 col-lg-4 team-2">
-                            <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
-                                <a href="#">
-                                    <img class="shadow-lg" src="https://avatars3.githubusercontent.com/u/804684?v=3&s=150" alt="...">
-                                </a>
-                                <h5>Marcel Pociot <small>Creator of <a href="#">BotMan</a></small></h5>
-                                <div class="social social-gray">
-                                    <a class="social-github" href="#"><i class="fa fa-github"></i></a>
-                                    <a class="social-twitter" href="#"><i class="fa fa-twitter"></i></a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-12 col-md-6 col-lg-4 team-2">
-                            <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
-                                <a href="#">
-                                    <img class="shadow-lg" src="https://avatars3.githubusercontent.com/u/19746749?v=3&s=150" alt="...">
-                                </a>
-                                <h5>Lalit Vijay  <small>Backend Lead at <a href="">StyleDotMe</a></small></h5>
-                                <div class="social social-gray">
-                                    <a class="social-github" href="#"><i class="fa fa-github"></i></a>
-                                    <a class="social-twitter" href="#"><i class="fa fa-twitter"></i></a>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="col-12 col-md-6 col-lg-4 team-2">
-                            <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
-                            <a href="#">
-                                <img class="shadow-lg" src="https://avatars3.githubusercontent.com/u/12281411?v=3&s=150" alt="...">
-                            </a>
-                            <h5>Frederick Vanbrabant  <small>Software Engineer <a href="">madewithlove</a></small></h5>
-                            <div class="social social-gray">
-                                <a class="social-github" href="#"><i class="fa fa-github"></i></a>
-                                <a class="social-twitter" href="#"><i class="fa fa-twitter"></i></a>
-                            </div>
-                            </div>
-                        </div>
-
-
-                        <div class="col-12 col-md-6 col-lg-4 team-2">
-                            <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
-                            <a href="#">
-                                <img class="shadow-lg" src="https://avatars3.githubusercontent.com/u/1410358?v=3&s=150" alt="...">
-                            </a>
-                            <h5>Hannes Van De Vreken  <small>Software Engineer at <a href="">getstreamio</a></small></h5>
-                            <div class="social social-gray">
-                                <a class="social-github" href="#"><i class="fa fa-github"></i></a>
-                                <a class="social-twitter" href="#"><i class="fa fa-twitter"></i></a>
-                            </div>
-                            </div>
-                        </div>
-                        --}}
                         
                     </div>
 
                     <div class="row gap-y">
+
+                        <div class="col-12 col-md-3 col-lg-4 team-2">
+                            <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
+                                <a href="https://vrajroham.me">
+                                    <img class="shadow-lg" src="https://avatars0.githubusercontent.com/u/12662173?s=150&v=3" alt="...">
+                                </a>
+                                <h5>Vaibhavraj Roham <small>Product Engineer at <a href="https://myvizo.com/">MYVIZO</a></small></h5>
+                                <div class="social social-gray">
+                                    <a class="social-github" href="https://github.com/vrajroham"><i class="fa fa-github"></i></a>
+                                    <a class="social-twitter" href="https://twitter.com/vrajroham"><i class="fa fa-twitter"></i></a>
+                                </div>
+                            </div>
+                        </div>
 
                         <div class="col-12 col-md-3 col-lg-4 team-2">
                             <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
