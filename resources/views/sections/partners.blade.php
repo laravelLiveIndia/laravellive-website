052363a8ec1ec0bc96d829463d31c87c38668bcc
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2018-02-17 22:50:16 +0530

    rounded borders fixed

--- resources/views/sections/partners.blade.php
+++ resources/views/sections/partners.blade.php
@@ -10,7 +10,7 @@
             <div class="col-12 col-md-3 col-lg-4 team-2">
                 <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
                     <a href="https://laravel.com/certification/">
-                        <img class="shadow-lg" src="https://pbs.twimg.com/profile_images/902849483121115136/Mk6HortE_400x400.jpg" alt="..." style="width: 150px; height: 150px;">
+                        <img class="shadow-lg part" src="https://pbs.twimg.com/profile_images/902849483121115136/Mk6HortE_400x400.jpg" alt="..." style="width: 150px; height: 150px;">
                     </a>
                     <h5>
                         Laravel Certification
@@ -22,7 +22,7 @@
             <div class="col-12 col-md-3 col-lg-4 team-2">
                 <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
                     <a href="https://flock.com/in/">
-                        <img class="shadow-lg" src="https://pbs.twimg.com/profile_images/881147326739263488/5iU0I0wb_400x400.jpg" alt="..." style="width: 150px; height: 150px;">
+                        <img class="shadow-lg part" src="https://pbs.twimg.com/profile_images/881147326739263488/5iU0I0wb_400x400.jpg" alt="..." style="width: 150px; height: 150px;">
                     </a>
                     <h5>
                         Flock
@@ -34,7 +34,7 @@
             <div class="col-12 col-md-3 col-lg-4 team-2">
                 <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
                     <a href="https://innov8.work/">
-                        <img class="shadow-lg" src="https://pbs.twimg.com/profile_images/695676635379474432/iyJRbGvN_400x400.jpg" alt="..." style="width: 150px; height: 150px;">
+                        <img class="shadow-lg part" src="https://pbs.twimg.com/profile_images/695676635379474432/iyJRbGvN_400x400.jpg" alt="..." style="width: 150px; height: 150px;">
                     </a>
                     <h5>
                         Innov8
@@ -48,7 +48,7 @@
             <div class="col-12 col-md-3 col-lg-4 team-2">
                 <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
                     <a href="http://www.lbsim.ac.in/">
-                        <img class="shadow-lg" src="http://www.lbsim.ac.in/Uploads/SmallImages/75newsimg_8announce_5announce_freesms_logo.png" alt="..." style="width: 150px; height: 150px;">
+                        <img class="shadow-lg part" src="http://www.lbsim.ac.in/Uploads/SmallImages/75newsimg_8announce_5announce_freesms_logo.png" alt="..." style="width: 150px; height: 150px;">
                     </a>
                     <h5>
                         Lal Bahadur Shastri Institute of Management
