0de13140ff7910698ec66620559e717bb96c7768
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2018-03-17 08:32:48 +0530

    added community partners

--- resources/views/sections/partners.blade.php
+++ resources/views/sections/partners.blade.php
@@ -31,8 +31,30 @@
                 </div>
             </div>
 
-        </div>
-        <div class="row gap-y">
+            <div class="col-12 col-md-3 col-lg-4 team-2">
+                <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
+                    <a href="https://innov8.work/">
+                        <img class="shadow-lg part" src="https://pbs.twimg.com/profile_images/695676635379474432/iyJRbGvN_400x400.jpg" alt="..." style="width: 150px; height: 150px;">
+                    </a>
+                    <h5>
+                        Innov8
+                        <small>Community Partner</small>
+                    </h5>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 col-lg-4 team-2">
+                <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
+                    <a href="http://www.91springboard.com/">
+                        <img class="shadow-lg part" src="{{ asset('images/spring-board.png') }}" alt="..." style="width: 150px; height: 150px;">
+                    </a>
+                    <h5>
+                        Springboard 91
+                        <small>Community Partner</small>
+                    </h5>
+                </div>
+            </div>
+
             <div class="col-12 col-md-3 col-lg-4 team-2">
                 <div class="shadow-lg rounded-lg bg-white mr-30 ml-30 p-30">
                     <a href="http://www.lbsim.ac.in/">
